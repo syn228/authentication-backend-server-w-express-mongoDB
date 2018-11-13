7cb2d7beaa225b30b7f94574a0bb4e45c3099ed5
Aaron Na <syn228@nyu.edu>
2018-11-12 19:47:56 -0800

    added nodemon and set up express middleware morgan and bodyParser

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const morgan = require('morgan')
 
 const app = express();
 
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*'}))
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app)
